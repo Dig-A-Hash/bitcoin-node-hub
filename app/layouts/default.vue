6448cb2bfc2c7d2d6a10c89eebd049f82c7485aa
KidSysco <kidsysco@gmail.com>
2025-09-14 22:49:35 -0500

    update layout, add nav menu on lg breakpoint

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -1,48 +1,73 @@
 <template>
-  <div>
-    <!-- Header -->
-    <header
-      class="dark:bg-elevated/50 light:bg-elevated/90 border-b border-accented/50 flex justify-between items-center"
+  <div class="flex min-h-screen">
+    <!-- Sidebar for large screens -->
+    <aside
+      class="hidden lg:block w-64 border-r border-accented/50 dark:bg-elevated/50 light:bg-elevated/60"
     >
-      <div class="flex items-center">
-        <UButton
-          color="secondary"
-          variant="ghost"
-          @click="toggleDrawer"
-          class="mr-4 rounded-none p-3"
-        >
-          <UIcon size="28" name="solar:hamburger-menu-linear" />
-        </UButton>
+      <div class="flex items-center p-3 justify-center">
         <UIcon
           size="32"
           name="bitcoin-icons:bitcoin-circle-filled"
           class="dark:text-orange-400 light:text-orange-700/80 mr-1"
         ></UIcon>
-        <span class="text-lg font-semibold"> Bitcoin Node Hub</span>
+        <span class="text-lg font-semibold">Bitcoin Node Hub</span>
       </div>
-      <ColorModeToggle />
-    </header>
-
-    <!-- Nav Drawer -->
-    <USlideover
-      aria-describedby="undefined"
-      v-model:open="isOpen"
-      :ui="{ content: 'w-64', body: 'p-2 sm:p-2' }"
-      side="left"
-      title="Bitcoin Node Hub"
-    >
-      <template #body>
+      <div class="p-2 pt-0">
         <UNavigationMenu
           :items="navItems"
           orientation="vertical"
           color="secondary"
           class="w-full"
         />
-      </template>
-    </USlideover>
+      </div>
+    </aside>
 
-    <!-- Main content -->
-    <slot />
+    <!-- Main content area -->
+    <div class="flex-1">
+      <!-- Header -->
+      <header
+        class="dark:bg-elevated/50 light:bg-elevated/60 border-b border-accented/50 flex justify-between items-center"
+      >
+        <div class="flex items-center">
+          <UButton
+            color="secondary"
+            variant="ghost"
+            @click="toggleDrawer"
+            class="mr-4 rounded-none p-3 lg:hidden"
+          >
+            <UIcon size="28" name="solar:hamburger-menu-linear" />
+          </UButton>
+          <UIcon
+            size="32"
+            name="bitcoin-icons:bitcoin-circle-filled"
+            class="dark:text-orange-400 light:text-orange-700/80 mr-1 lg:hidden"
+          ></UIcon>
+          <span class="text-lg font-semibold lg:hidden">Bitcoin Node Hub</span>
+        </div>
+        <ColorModeToggle />
+      </header>
+
+      <!-- Nav Drawer for smaller screens -->
+      <USlideover
+        v-model:open="isOpen"
+        :ui="{ content: 'w-64', body: 'p-2 sm:p-2' }"
+        side="left"
+        title="Bitcoin Node Hub"
+        class="lg:hidden"
+      >
+        <template #body>
+          <UNavigationMenu
+            :items="navItems"
+            orientation="vertical"
+            color="secondary"
+            class="w-full"
+          />
+        </template>
+      </USlideover>
+
+      <!-- Main content -->
+      <slot />
+    </div>
   </div>
 </template>
 
@@ -58,7 +83,17 @@ const navItems = ref<NavigationMenuItem[]>([
   {
     label: 'Dashboard',
     icon: 'material-symbols:dashboard',
+    class: 'p-3 my-1',
     to: '/',
+    onSelect: () => {
+      isOpen.value = false; // Close the slideover
+    },
+  },
+  {
+    label: 'Search',
+    icon: 'material-symbols:search',
+    class: 'p-3 my-1',
+    to: '/search',
     onSelect: () => {
       isOpen.value = false; // Close the slideover
     },
@@ -66,6 +101,16 @@ const navItems = ref<NavigationMenuItem[]>([
   {
     label: 'Settings',
     icon: 'material-symbols:settings',
+    class: 'p-3 my-1',
+    to: '/settings',
+    onSelect: () => {
+      isOpen.value = false; // Close the slideover
+    },
+  },
+  {
+    label: 'About',
+    icon: 'material-symbols:person-play-rounded',
+    class: 'p-3 my-1',
     to: '/settings',
     onSelect: () => {
       isOpen.value = false; // Close the slideover
